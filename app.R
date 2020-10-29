2374b0bcc9b40941f320efa7ce9d22eea8688b9a
Rebecca Furbeck <rafurbeck@gmail.com>
2020-10-29 13:17:28 -0500

    Select Ingredients

--- app.R
+++ app.R
@@ -9,32 +9,23 @@
 
 library(shiny)
 
+#Put data in
+cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/cocktails.csv')
+ingredients<-unique(cocktails$ingredient)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Choose Your Drink!"),
 
     # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    selectizeInput(
+        'Ingredients', label = NULL, choices = ingredients,
+        options = list(create = TRUE)
     )
 )
 
-#Put data in
-cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/cocktails.csv')
-unique(cocktails$ingredient)
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
@@ -48,6 +39,7 @@ server <- function(input, output) {
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
     })
 }
+
 
 # Run the application 
 shinyApp(ui = ui, server = server)
