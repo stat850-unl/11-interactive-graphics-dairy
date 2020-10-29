8c3136f739bbf7c2940bd9c28dcf8935cf1d1a6e
kbuse2 <kassidy.buse@huskers.unl.edu>
2020-10-29 14:11:53 -0500

    Changes

--- RecipeTable.R
+++ RecipeTable.R
@@ -1,10 +1,27 @@
-server <- function(input, output) {recipes <- reactive({
-  cocktails %>%
-    group_by(drink) %>%
-    select(drink, ingredient, measure) %>%
-    summarise("Midnight Cowboy")
-})
-output$table1 <- renderTable({recipes()
-})
+library(shiny)
+library(tidyverse)
+
+ui <- fluidPage(
+
+  selectInput(inputId = "choice",
+              label = "Select drink",
+              choices = names(cocktails$drink),
+              selected = c("'57 Chevy with a White License Plate")),
+
+  tableOutput(outputId = "recipe")
+)
+
+server <- function(input, output) {
+  recipe <- reactive({
+    req(input$choice)
+    cocktails %>% group_by(!! sym(input$choice)) %>% select(drink, ingredient, measure) %>% summarise(recipe = n())
+  })
+
+  # Headcount
+  output$fund <- renderTable({
+    recipe()
+  })
 }
+
+shinyApp(ui = ui, server = server)
 
