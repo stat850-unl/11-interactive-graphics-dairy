1a78ca1413dc713491d5366632eafd2949427b01
kbuse2 <kassidy.buse@huskers.unl.edu>
2020-10-30 15:31:05 -0500

    fff

--- app.R
+++ app.R
@@ -17,7 +17,7 @@ ingredients<-unique(cocktails$ingredient)
 categoryops<-unique(cocktails$category)
 cocktails$url <- paste0("<img src='",cocktails$drink_thumb,"'height=100>",cocktails$drink_thumb,"</img>")
 
-# Define UI for application 
+# Define UI for application
 ui <- fluidPage(
 
      # Application title
@@ -25,20 +25,20 @@ ui <- fluidPage(
 
     # Sidebar layout with input and output definitions ----
     sidebarLayout(
-        
-        # Sidebar panel for inputs 
+
+        # Sidebar panel for inputs
         sidebarPanel(
-            
+
             # Inputs ----
             #Checkboxes for category
             checkboxGroupInput("checkGroup",
                                h3("Category of Cocktail"),
                                choices = categoryops
             ),
-            
-            # br() element to introduce extra vertical spacing 
+
+            # br() element to introduce extra vertical spacing
             br(),
-            
+
             #Search by Ingredient
             h3("Choose your ingredients of interest"),
             # Sidebar with a slider input for number of bins
@@ -46,18 +46,18 @@ ui <- fluidPage(
                 'Ingredients', label = "Scroll or Type Entry", choices = ingredients,
                 options = list(create = TRUE)
             )
-            
+
         ),
-        
-        # Main panel for displaying outputs 
+
+        # Main panel for displaying outputs
         mainPanel(
-            
-            # Output: Tabset w/ plot, summary, and table 
+
+            # Output: Tabset w/ plot, summary, and table
             tabsetPanel(type = "tabs",
                         tabPanel("Photos", DT::dataTableOutput('mytable')),
                         tabPanel("Custom Recipe Book", tableOutput("tbl"))
             )
-            
+
         )
     )
 )
@@ -66,21 +66,21 @@ ui <- fluidPage(
 # Define server logic
 server = function(input, output) {
     # Make reactive dataset because renderDataTable got mad without it in a reactive statement but I need it to render html
-    dat <-  reactive({cocktails %>% 
-            dplyr::filter(category %in% input$checkGroup & ingredient %in%input$Ingredients) %>% 
+    dat <-  reactive({cocktails %>%
+            dplyr::filter(category %in% input$checkGroup & ingredient %in%input$Ingredients) %>%
             select(drink,url) %>% distinct(drink,url)
     })
-    
+
     #Make table for photos
     output$mytable <- DT::renderDataTable({
-        
+
         DT::datatable(dat(), escape = FALSE)
     })
-    
+
     #Make table for recipes
     output$tbl <- renderTable({
-        cocktails %>% 
-            filter(category %in% (input$checkGroup)) %>% 
+        cocktails %>%
+            filter(category %in% (input$checkGroup)) %>%
             select(drink, ingredient, measure)
     })
 }
